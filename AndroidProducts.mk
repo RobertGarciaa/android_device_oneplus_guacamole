0de1cadaaa4cb84b375a5d8ea161bc9bfaed47fa
RobertGarciaa <chae0218@naver.com>
2023-12-29 22:47:53 +0900

    guacamole: Fix miss typo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_guacamole.mk
     
 COMMON_LUNCH_CHOICES := \
-    aosp_renoir-user \
-    aosp_renoir-userdebug \
-    aosp_renoir-eng
+    aosp_guacamole-user \
+    aosp_guacamole-userdebug \
+    aosp_guacamole-eng
